43aa3017f5bad5d9710527a9872823a48c7f7c2c
Tobi <pix1126@dlsud.edu.ph>
2026-09-09 17:53:19 +0800

    feat: apply Angular Material button and styling - 5

--- ALICA/src/app/padolina/padolina.ts
+++ ALICA/src/app/padolina/padolina.ts
@@ -1,7 +1,8 @@
 import { Component, signal } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
-  imports: [],
+  imports: [MatButtonModule],
   selector: 'app-padolina',
   styleUrl: './padolina.scss',
   templateUrl: './padolina.html',
