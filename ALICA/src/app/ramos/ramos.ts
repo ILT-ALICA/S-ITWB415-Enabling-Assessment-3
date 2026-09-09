86053fc23291887af2f16ee6af0acdbd495cf986
ramos-lenard <ramoslenardkristan@gmail.com>
2026-09-09 12:01:15 +0800

    Task 5: apply angular material button and styling

--- ALICA/src/app/ramos/ramos.ts
+++ ALICA/src/app/ramos/ramos.ts
@@ -1,7 +1,8 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  imports: [],
+  imports: [MatButtonModule],
   selector: 'app-ramos',
   styleUrl: './ramos.scss',
   templateUrl: './ramos.html',
