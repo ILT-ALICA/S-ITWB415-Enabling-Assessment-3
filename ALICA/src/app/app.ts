933e33843eb7a4d9fbb798ea2ea4c4c4a4764091
Layug-Charlz <charlzsrsly@gmail.com>
2026-09-09 10:35:13 +0800

    add component into the root App component

--- ALICA/src/app/app.ts
+++ ALICA/src/app/app.ts
@@ -3,9 +3,10 @@ import { Ramos } from './ramos/ramos';
 import { Umandal } from './umandal/umandal';
 import { Martin } from './martin/martin';
 import { Padolina } from './padolina/padolina';
+import { Layug } from './layug/layug';
 
 @Component({
-  imports: [Ramos, Umandal, Martin, Padolina],
+  imports: [Ramos, Umandal, Martin, Padolina, Layug],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
