f26d99535aa2e695a208d1c0302c490a32cfd953
Tobi <pix1126@dlsud.edu.ph>
2026-09-09 17:14:44 +0800

    feat: create Padolina component - 1

---
+++ ALICA/src/app/padolina/padolina.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-padolina',
+  styleUrl: './padolina.scss',
+  templateUrl: './padolina.html',
+})
+export class Padolina {}
