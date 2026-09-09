834be24225be6099b4915062df022e909fa8432c
Tobi <pix1126@dlsud.edu.ph>
2026-09-09 17:45:01 +0800

    feat: add PAdolina description with event binding - 4

--- ALICA/src/app/padolina/padolina.ts
+++ ALICA/src/app/padolina/padolina.ts
@@ -8,4 +8,11 @@ import { Component, signal } from '@angular/core';
 })
 export class Padolina {
   name = signal('Ian Charles Padolina');
+  imagePath = signal('assets/images/padolina.jpg');
+  description = signal('');
+
+  showDescription() {
+    this.description.set('I am an Information Technology student at DLSU-D who enjoys graphic design, content creation, and competitive running. I am also active in student organizations like Lasallian Student Ambassadors and Samahang Lasalyano.');
+  }
+
 }
