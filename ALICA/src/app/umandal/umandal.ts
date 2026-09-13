590477c6dbdf7ce12df79a1616ad84696bbd168b
Umandal-Alen <amc.umandal@gmail.com>
2026-09-13 21:50:25 +0800

    fix: format code to guidelines

--- ALICA/src/app/umandal/umandal.ts
+++ ALICA/src/app/umandal/umandal.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MemberPreview } from '../shared/member-preview/member-preview';
 
 @Component({
-  imports: [MatButtonModule, MemberPreview],
+  imports: [MemberPreview],
   selector: 'app-umandal',
   styleUrl: './umandal.scss',
   templateUrl: './umandal.html',
@@ -13,9 +12,9 @@ export class Umandal {
   imagePath = signal('assets/images/umandal.png');
   description = signal(
     'I specialize in web and full-stack development, working across ' +
-      'both frontend and backend with modern tools and tech stacks, ' +
-      'focusing on building practical applications with clean UI/UX, ' +
-      'thoughtful database design, and solutions that solve real-world ' +
-      'problems.'
+    'both frontend and backend with modern tools and tech stacks, ' +
+    'focusing on building practical applications with clean UI/UX, ' +
+    'thoughtful database design, and solutions that solve real-world ' +
+    'problems.',
   );
 }
