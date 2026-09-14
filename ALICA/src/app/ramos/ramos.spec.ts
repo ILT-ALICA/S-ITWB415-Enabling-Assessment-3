880ae151823c692c61c92a13262bf0ce83f0d5f4
ramos-lenard <ramoslenardkristan@gmail.com>
2026-09-14 15:58:54 +0800

    feat: Ramos updated component for member-preview

--- ALICA/src/app/ramos/ramos.spec.ts
+++ ALICA/src/app/ramos/ramos.spec.ts
@@ -27,9 +27,13 @@ describe('Ramos', () => {
     expect(component.imagePath()).toBe('assets/images/ramos.png');
   });
 
-  it('should update description on showDescription', () => {
-    expect(component.description()).toBe('');
-    component.showDescription();
+    it('should have valid description', () => {
     expect(component.description()).toContain('functional web apps');
+  });
+
+  it('should render the member preview component', () => {
+    const previewElement =
+      fixture.nativeElement.querySelector('app-member-preview');
+    expect(previewElement).toBeTruthy();
   });
 });
--- ALICA/src/app/ramos/ramos.ts
+++ ALICA/src/app/ramos/ramos.ts
@@ -1,8 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MemberPreview } from '../shared/member-preview/member-preview';
 @Component({
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MemberPreview],
   selector: 'app-ramos',
   styleUrl: './ramos.scss',
   templateUrl: './ramos.html',
@@ -10,13 +10,8 @@ import { MatButtonModule } from '@angular/material/button';
 export class Ramos {
   name = signal('Lenard Kristan A. Ramos');
   imagePath = signal('assets/images/ramos.png');
-  description = signal('');
-
-  showDescription() {
-    this.description.set(
-      'With full-stack innovations at the core of my work, I create ' +
-      'functional web apps using contemporary frameworks, strong APIs, ' +
-      'and efficient databases.',
-    );
-  }
+  description = signal(
+    'With full-stack innovations at the core of my work, I create ' +
+    'functional web apps using contemporary frameworks, strong APIs, ' +
+    'and efficient databases.');
 }
